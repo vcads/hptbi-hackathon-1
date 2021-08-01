43d11e46739907181dec049e6a70da25b0be7c77
vcads <87100027+vcads@users.noreply.github.com>
2021-07-31 22:04:39 -0400

    Update fss_model.py

--- fss_model.py
+++ fss_model.py
@@ -8,15 +8,30 @@
 # Return:
 #
 import numpy as np
-from sklearn import linear_model
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 def fss_model(data):
     ############################################################################
     # User code starts here
-    xmat = data[["age", "female", "icpyn1"]]
-    yvec = data[["fss_total"]]
-    rtn = linear_model.LinearRegression()
-    rtn.fit(xmat, yvec)
+    
+    rtn = []
+	dataset = data.to_numpy()
+	fss_datasets = [dataset[:, 0:7], dataset[:, 7:14], dataset[:, 14:29], dataset[:, 29:50], dataset[:, 50:71], dataset[:, 71:88]]
+	fss_scores = dataset[:, 88:94]
+
+	for i in range(len(fss_datasets)):
+		target = fss_scores[:, i]
+		target_data = []
+		for j in range(len(target)):
+			target_data.append(target[j])
+
+		x_train, x_test, y_train, y_test = train_test_split(fss_datasets[i], target_data, test_size = 0.3, random_state = 100)
+		classifier = RandomForestClassifier(max_depth = 6, min_samples_leaf = 1, min_samples_split = 3, class_weight = 'balanced', bootstrap = False, max_features = "auto", random_state = 150)
+		classifier.fit(x_train, y_train)
+		rtn.append(classifier)
+        
     # User code ends here
     ############################################################################
     return rtn
@@ -33,8 +48,22 @@ def fss_model(data):
 def predict_fss(model, newdata):
     ############################################################################
     # user defined code starts here
-    xmat = newdata[["age", "female", "icpyn1"]]
-    return model.predict(xmat).astype(int)
+    dataset = newdata.to_numpy()
+	fss_datasets = [dataset[:, 0:7], dataset[:, 7:14], dataset[:, 14:29], dataset[:, 29:50], dataset[:, 50:71], dataset[:, 71:88]]
+	fss_scores = dataset[:, 88:94]
+	predictions = []
+	for i in range(len(models)):
+		y_pred = models[i].predict(fss_datasets[i])
+		predictions.append(y_pred)
+
+	fss_vals = []
+	for i in range(len(predictions[0])):
+		fss_sum = 0
+		for j in range(len(predictions)):
+			fss_sum += predictions[j][i]
+		fss_vals.append(fss_sum)
+
+	return fss_vals
 
 ################################################################################
 #                                 End of File
